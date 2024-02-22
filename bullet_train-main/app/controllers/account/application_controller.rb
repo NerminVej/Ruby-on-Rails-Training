e44f1884f53464d3b95571ef370bbf070736919b
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-02-22 22:15:20 +0100

    deleted some more comments

--- bullet_train-main/app/controllers/account/application_controller.rb
+++ bullet_train-main/app/controllers/account/application_controller.rb
@@ -2,10 +2,8 @@ class Account::ApplicationController < ApplicationController
   include Account::Controllers::Base
 
   def ensure_onboarding_is_complete
-    # First check that Bullet Train doesn't have any onboarding steps it needs to enforce.
     return false unless super
 
-    # Most onboarding steps you'll add should be skipped if the user is adding a team or accepting an invitation ...
     unless adding_team? || accepting_invitation?
       # So, if you have new onboarding steps to check for an enforce, do that here:
     end
