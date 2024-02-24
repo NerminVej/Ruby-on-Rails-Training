0152abc88ef4b04bb766696adbbe7bb7d515fd9d
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-02-24 13:03:33 +0100

    super cafolding have to look up

--- bullet_train-main/app/controllers/account/users_controller.rb
+++ bullet_train-main/app/controllers/account/users_controller.rb
@@ -6,6 +6,7 @@ class Account::UsersController < Account::ApplicationController
   def permitted_fields
     [
       # 🚅 super scaffolding will insert new fields above this line.
+      # Have to look up
     ]
   end
 
